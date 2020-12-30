dfb2fea60c236d13800115cc7dd6f18a8c798203
Hyunseung Jeon <dogdriip@gmail.com>
2020-12-30 17:01:14 +0900

    init

---
+++ src/main/scala/Ex_1_1.scala
@@ -0,0 +1,5 @@
+object Ex_1_1 {
+  def main(args: Array[String]): Unit = {
+    println("Hello, Scala!")
+  }
+}
---
+++ src/main/scala/Ex_2_1.scala
@@ -0,0 +1,9 @@
+object Ex_2_1 {
+  def main(args: Array[String]): Unit = {
+    var a = "Variable."
+    val b = "Final variable."
+    a = "Can it be mutable?"
+    b = "Can it be mutable?"
+    println(a)
+  }
+}

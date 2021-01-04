067d2817f416ba67073abdd316bf6f5339b75318
Hyunseung Jeon <dogdriip@gmail.com>
2021-01-04 19:57:56 +0900

    Ex7

---
+++ src/main/scala/ex7/Ex_7_10.scala
@@ -0,0 +1,11 @@
+package ex7
+
+object Ex_7_10 {
+  def main(args: Array[String]): Unit = {
+    val list = List("a", "b", "c")
+    val i = list.iterator
+    while (i.hasNext) {
+      println(i.next())
+    }
+  }
+}
